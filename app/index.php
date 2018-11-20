2a475b50d04c1da0e17941da40ef05b14980498a
mischoko <michal.zak.misch@gmail.com>
2018-11-20 20:10:59 +0100

    Major fix

--- app/index.php
+++ app/index.php
@@ -1,11 +1,22 @@
-<?php
 
+<main class="mainWrapper">
+    <h1 class="logInHeader">Prihlásiť sa:</h1>
+    <div class="formField">
+        <label for="name">Meno: </label><br>
+            <input name="name" type="text" placeholder="username" required><br>
+        <label for="name">Heslo: </label><br>
+            <input name="pass" type="text" placeholder="password" required>
+    </div>
+</main>
+
+<!-- php db access -->
+
+<?php
 require 'start.php';
+require 'header.php';
 
 $pages = $db->query("
     SELECT id, code, name
     FROM products
 ")->fetchAll(PDO::FETCH_ASSOC);
-echo "test";
-
 ?>
