f721eaf2b7d421ec7f6edc12c907fd51bafea5e2
Misch <michal.zak.misch@gmail.com>
2018-11-20 14:33:32 +0100

    first commit

---
+++ app/start.php
@@ -0,0 +1,10 @@
+<?php
+
+ini_set('display_errors','On');
+
+define('APP_ROOT', __DIR__);
+define('BASE_URL', 'http://localhost:3000/sklad');
+
+$db = new PDO("mysql:host=mariadb101.websupport.sk;port=3312;dbname=dbsklad", "dbsklad", "ZmenS1Heslo!");
+
+?>
