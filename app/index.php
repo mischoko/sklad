d20050c48bc249d19d0e6341b45c6fa3b4d6c0fd
Misch <michal.zak.misch@gmail.com>
2018-11-21 12:08:02 +0100

    fix of the login system

--- app/index.php
+++ app/index.php
@@ -1,22 +1,39 @@
 
-<main class="mainWrapper">
-    <h1 class="logInHeader">Prihlásiť sa:</h1>
-    <div class="formField">
-        <label for="name">Meno: </label><br>
-            <input name="name" type="text" placeholder="username" required><br>
-        <label for="name">Heslo: </label><br>
-            <input name="pass" type="text" placeholder="password" required>
-    </div>
-</main>
-
 <!-- php db access -->
-
 <?php
 require 'start.php';
 require 'header.php';
 
-$pages = $db->query("
-    SELECT id, code, name
-    FROM products
-")->fetchAll(PDO::FETCH_ASSOC);
+session_start();
+
+if(isset($_POST['name'])){
+    // Getting variables
+    $name     = !empty($_POST['name']) ? htmlentities($_POST['name']) : "";
+    $pass     = !empty($_POST['pass']) ? htmlentities($_POST['pass']) : "";
+
+    $sql = "SELECT id FROM user WHERE username = ? and password =?";
+    $query   = $db->prepare($sql);
+    $query->execute([$name, $pass]);
+    $results = $query->fetchAll();
+
+    if (count($results) === 1){
+        $_SESSION['username'] = $name;
+        header('location:login.php');
+        
+    }
+}
+
 ?>
+
+<main class="mainWrapper">
+    <h1 class="logInHeader">Prihlásiť sa:</h1>
+    <div class="formField">
+        <form method="POST" name="login" action="index.php">
+            <label for="name">Meno: </label><br>
+                <input name="name" type="text" placeholder="username" required><br>
+            <label for="name">Heslo: </label><br>
+                <input name="pass" type="text" placeholder="password" required><br>
+                <button name="submit" value="submit" type="submit" >Submit</button>
+        </form>    
+    </div>
+</main>
